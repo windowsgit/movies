a1af682b049e8b366e7ac06795c529725caa271e
vagrant <vagrant@kellogg-dev-box>
2014-04-22 15:47:48 +0000

    Display correct year and name of director for each movie.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421233451) do
+ActiveRecord::Schema.define(version: 20140422140917) do
 
   create_table "directors", force: true do |t|
     t.string "name"

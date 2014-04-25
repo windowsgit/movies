5898de04828a606e735fe5552649db3e324cfa67
vagrant <vagrant@kellogg-dev-box>
2014-04-25 19:09:19 +0000

    Make the directors/show page smart by expecting a querystring parameter in the url.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422140917) do
+ActiveRecord::Schema.define(version: 20140425141544) do
 
   create_table "directors", force: true do |t|
     t.string "name"

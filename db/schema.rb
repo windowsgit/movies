5c3705b99d70363e8dec0780363e17a2eae79baf
vagrant <vagrant@kellogg-dev-box>
2014-04-29 15:42:03 +0000

    Begin to use associations to make the movie pages smarter.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429014304) do
+ActiveRecord::Schema.define(version: 20140429130701) do
 
   create_table "actors", force: true do |t|
     t.string "name"

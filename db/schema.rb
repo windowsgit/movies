175cf8b92464a949e28c59949722cb0dd71261ab
vagrant <vagrant@kellogg-dev-box>
2014-04-30 01:10:27 +0000

    remove hint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429130701) do
+ActiveRecord::Schema.define(version: 20140430010743) do
 
   create_table "actors", force: true do |t|
     t.string "name"

2fe1a13de0d2809e61dd3907d48c68e842252db9
vagrant <vagrant@kellogg-dev-box>
2014-05-06 21:00:19 +0000

    Fix the movies/show view, and add a DirectorsController class

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,9 @@
+class DirectorsController < ApplicationController
+
+  # Try commenting out or removing this code entirely
+  # to see what would happen.
+  def destroy
+    redirect_to "http://www.apple.com"
+  end
+
+end
